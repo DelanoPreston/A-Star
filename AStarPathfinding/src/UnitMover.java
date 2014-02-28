73fa96e5574cab45ceac6ebacbbfe84d25c8568a
Preston Delano <Delanopreston@gmail.com>
2014-02-27 22:22:07 -0700

    First Commit

---
+++ AStarPathfinding/src/UnitMover.java
@@ -0,0 +1,14 @@
+
+public class UnitMover implements Mover{
+	 private int type;
+	  
+	  public UnitMover(int type)
+	  {
+	    this.type = type;
+	  }
+	  
+	  public int getType()
+	  {
+	    return this.type;
+	  }
+}
